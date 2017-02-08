48bf56cc435f032805aa017652325e2a67f03c3f
Daniel <danielems3@outlook.com>
2017-02-08 13:06:44 -0500

    Copy, rename, and redeclare structs.h from codec

---
+++ zerg_functions.h
@@ -0,0 +1,18 @@
+#ifndef ZERG_FUNCTIONS_H
+#define ZERG_FUNCTIONS_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+
+extern union PayloadStructs *zerg;
+void messFunction(union PayloadStructs *zerg);
+void statFunction(union PayloadStructs *zerg);
+void commFunction(union PayloadStructs *zerg);
+void gpsFunction(union PayloadStructs *zerg);
+
+
+#endif
