ba708dabb30761c227f7369c0c8858c958824013
Daniel <danielems3@outlook.com>
2017-02-13 14:16:15 -0500

    Update new .h file declarations

--- zerg_functions.c
+++ zerg_functions.c
@@ -1,5 +1,6 @@
 #include "zerg_functions.h"
 #include "zergmap_functions.h"
+#include "zerg_list.h"
 #include <stdbool.h>
 
 void
@@ -28,11 +29,13 @@ statFunction(union PayloadStructs *zerg)
 
     race = raceId(type);
 
+	/*
     printf("Name    : %s\n", zerg->stat.name);
     printf("HP      : %d/%d\n", zerg->stat.hit_armor, zerg->stat.max_type);
     printf("Type    : %s\n", race);
     printf("Armor   : %d\n", armor);
     printf("Maxspeed: %fm/s\n", speedy);
+	*/
     free(zerg);
 }
 
@@ -91,42 +94,37 @@ gpsFunction(union PayloadStructs *zerg)
     uint64_t lat = doub_flip(&zerg->gps.lat_first, &zerg->gps.lat_second);
     double latitude = doub_converter(&lat);
 
-    if (latitude >= 1)
+    if (latitude < 1)
     {
-        printf("latitude : %lf deg. N\n", latitude);
+        latitude *= (-1);
     }
-    else
-    {
-        printf("latitude : %lf deg. S\n", latitude * (-1));
-    }
-
+   
     uint64_t lon = doub_flip(&zerg->gps.long_first, &zerg->gps.long_second);
     double longitude = doub_converter(&lon);
 
     if (longitude >= 1)
     {
-        printf("longitude: %lf deg. E\n", longitude);
-    }
-    else
-    {
-        printf("longitude: %lf deg. W\n", longitude * (-1));
-    }
+        longitude *= (-1);
+	}
+  
+	
 
     float altitude = converter(&zerg->gps.altitude);
 
-    printf("altitude : %.1fm\n", altitude * 1.8288);
+    //printf("altitude : %.1fm\n", altitude * 1.8288);
 
     float bearing = converter(&zerg->gps.bearing);
 
-    printf("bearing  : %f deg.\n", bearing);
+    //printf("bearing  : %f deg.\n", bearing);
 
-    float speed = converter(&zerg->gps.speed);
+    //float speed = converter(&zerg->gps.speed);
 
-    printf("speed    : %.fkm/h\n", speed * 3.6);
+    //printf("speed    : %.fkm/h\n", speed * 3.6);
 
-    float accuracy = converter(&zerg->gps.accuracy);
+    //float accuracy = converter(&zerg->gps.accuracy);
 
-    printf("accuracy : %.fm\n", accuracy);
+    //printf("accuracy : %.fm\n", accuracy);
+	
     free(zerg);
-
+	return 
 }
