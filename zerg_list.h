69079ed9c2a9fdd2b333aebc6971e1654fcee598
Daniel <danielems3@outlook.com>
2017-02-14 13:10:35 -0500

    add the prototype to the print adj function

--- zerg_list.h
+++ zerg_list.h
@@ -21,14 +21,15 @@ typedef struct vertex{
 
 typedef struct edge{
 	int id;
-	int weight;
+	double weight;
 	struct edge* next;
 }edge;
 
 vertex *insertVertex(vertex *front, int id, double lon, double lat, float alt);
-edge * insertEdge(edge *front, int id, int weight);
+edge * insertEdge(edge *front, int id, double weight);
 double haversine(double lat1, double lon1, double lat2, double lon2);
 double pythagorean(float alt1, float alt2, double haversine);
+void printAdj(vertex *zergNode);
 void printList(vertex *front);
 void destroy(vertex *front);
 
