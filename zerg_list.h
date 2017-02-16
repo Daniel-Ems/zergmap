f090800f14597ed7db51f40884df4d156c683b81
Daniel <danielems3@outlook.com>
2017-02-16 16:48:24 -0500

    fix memory issue by rearranging .h files

--- zerg_list.h
+++ zerg_list.h
@@ -1,15 +1,16 @@
 #ifndef ZERG_LIST_H
 #define ZERG_LIST_H
 
-#include "zergmap.h"
-
-
 
 typedef struct vertex{
 	int id;
 	double lat;
 	double lon;
 	float alt;
+	int edges;
+	int remove;
+	int visited;
+	int from;
 	struct vertex *next;
 	struct edge* adj;
 }vertex;
@@ -17,9 +18,15 @@ typedef struct vertex{
 typedef struct edge{
 	int id;
 	double weight;
+	int remove;
+	int visited;
 	struct edge* next;
 }edge;
 
+void adjAdjust(vertex *zergNode, int adjId, int vertId );
+int removeSingle(vertex *zergNode);
+void printRemovals(vertex *front);
+int ListLength(vertex *front);
 vertex *insertVertex(vertex *front, int id, double lon, double lat, float alt);
 edge * insertEdge(edge *front, int id, double weight);
 double haversine(double lat1, double lon1, double lat2, double lon2);
