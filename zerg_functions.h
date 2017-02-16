a375ba718a4ffbb699e9aae843ef3f054968f964
Daniel <danielems3@outlook.com>
2017-02-16 16:47:13 -0500

    add healthtree declaration to my zerg_functions.h

--- zerg_functions.h
+++ zerg_functions.h
@@ -1,12 +1,15 @@
 #ifndef ZERG_FUNCTIONS_H
 #define ZERG_FUNCTIONS_H
 
-#include "zergmap.h"
-
+#include "healthTree.h"
+#include "zerg_list.h"
+#include "zergmap_functions.h"
+#include "zerg_functions.h"
+#include "dijkstra.h"
 
 extern union PayloadStructs *zerg;
 void messFunction(union PayloadStructs *zerg);
-void statFunction(union PayloadStructs *zerg);
+healthTree* statFunction(healthTree *tree, union PayloadStructs *zerg, uint32_t zergId);
 void commFunction(union PayloadStructs *zerg);
 struct vertex* gpsFunction(struct vertex *zergNode, 
 					union PayloadStructs *zerg, uint32_t id);
