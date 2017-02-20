fda0246707b4919925f4995650bc96c152e76f8a
Daniel <danielems3@outlook.com>
2017-02-20 12:50:39 -0500

    Begin final commits and cleaning

--- zerg_list.h
+++ zerg_list.h
@@ -9,6 +9,7 @@ typedef struct vertex{
 	float alt;
 	int edges;
 	int remove;
+	int total;
 	int visited;
 	int from;
 	struct vertex *next;
@@ -20,15 +21,22 @@ typedef struct edge{
 	double weight;
 	int remove;
 	int visited;
+	int total;
+	int from;
+	struct vertex *parent;
 	struct edge* next;
 }edge;
 
-void adjAdjust(vertex *zergNode, int adjId, int vertId );
+
+void gainWeight(vertex *zergNode);
+void adjAdjust(vertex *zergNode, int adjId);
 int removeSingle(vertex *zergNode);
+bool sameConnections(vertex *zergNode);
+int goodStrings(vertex *zergNode, int numNode);
 void printRemovals(vertex *front);
 int ListLength(vertex *front);
 vertex *insertVertex(vertex *front, int id, double lon, double lat, float alt);
-edge * insertEdge(edge *front, int id, double weight);
+edge * insertEdge(edge *front, int id, double weight, vertex *parent);
 double haversine(double lat1, double lon1, double lat2, double lon2);
 double pythagorean(float alt1, float alt2, double haversine);
 void printAdj(vertex *zergNode);
