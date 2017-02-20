5121395d205aa7409e95190ec9c8e008e3fe52f2
Daniel <danielems3@outlook.com>
2017-02-20 12:51:20 -0500

    begin final commits

--- zergmap.h
+++ zergmap.h
@@ -2,31 +2,9 @@
 #define ZERGMAP_H
 
 
-//#include "dijkstra.h"
-
-#include <stdlib.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include <arpa/inet.h>
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <sysexits.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
+#include "dijkstra.h"
 #include "zerg_list.h"
 #include "zergmap_functions.h"
 #include "zerg_functions.h"
+
 #endif
