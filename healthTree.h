5c3bb9e6c4badcdfaf5680767a05487119df9d62
Daniel <danielems3@outlook.com>
2017-02-20 12:59:44 -0500

    declarations for healthtree

---
+++ healthTree.h
@@ -0,0 +1,32 @@
+#ifndef HEALTHTREE_H
+
+#define HEALTHTREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sysexits.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+#include <stdbool.h>
+
+typedef struct healthTree
+{
+  int id;
+  double health;
+  struct healthTree *left;
+  struct healthTree *right;
+  int count;
+
+} healthTree;
+
+healthTree *insertTree(healthTree *tree, int id, double health);
+void print_tree (healthTree *tree, int lowHealth);
+void destroy_healthTree (healthTree * tree);
+
+#endif
