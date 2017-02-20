9d682fe3caf5b2755017685db9a703a280f16839
Daniel <danielems3@outlook.com>
2017-02-20 13:50:31 -0500

    Add enum for max health

--- healthTree.h
+++ healthTree.h
@@ -25,6 +25,7 @@ typedef struct healthTree
 
 } healthTree;
 
+enum{ MAX_HEALTH = 100};
 healthTree *insertTree(healthTree *tree, int id, double health);
 void print_tree (healthTree *tree, int lowHealth);
 void destroy_healthTree (healthTree * tree);
